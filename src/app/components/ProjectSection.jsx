8d3336069e59d3fc0c8ef01e5f6d9c423255c30c
LourdesHolzman <guadalupe.holzman@gmail.com>
2024-01-15 11:58:09 -0300

    Linted all files

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import ProjectCard from './ProjectCard';
+import React, { Fragment } from "react";
+import ProjectCard from "./ProjectCard";
 
 const projectsData = [
-    {
-        id:1,
-        title: "TUNA - Fotografía",
-        description:"",
-        image: "",
-        tag: ["All", "Web"],
-    },
-    {
-        id:2,
-        title: "TUNA - Fotografía",
-        description:"",
-        image: "",
-        tag: ["All", "Web"],
-    },
+  {
+    id: 1,
+    title: "TUNA - Fotografía",
+    description: "",
+    image: "",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 2,
+    title: "TUNA - Fotografía",
+    description: "",
+    image: "",
+    tag: ["All", "Web"],
+  },
 ];
 
 const ProjectSection = () => {
   return (
-    <>
-    <h2>Mis Proyectos</h2>
-    <div>{projectsData.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image}/>
-    )}
-    </div>
-    </>
-  )
+    <Fragment>
+      <h2>Mis Proyectos</h2>
+      <div>
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+          />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 export default ProjectSection;
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const ProjectsSection = () => {
-  return (
-    <><h2>ProjectsSection</h2><div></div></>
-  )
+  return <h2>ProjectsSection</h2>;
 };
 
 export default ProjectsSection;
